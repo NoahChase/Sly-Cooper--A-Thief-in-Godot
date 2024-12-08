17ff38557faa890f14f9d0ed808a30c4a180f938
NoahChase <noah.ryan.chase@gmail.com>
2024-12-08 10:59:04 -0800

    Update player_sly

--- Scripts/player__sly.gd
+++ Scripts/player__sly.gd
@@ -1,19 +1,29 @@
 extends CharacterBody3D
 
-
+#const
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+#enum
+enum state {FLOOR, AIR, TO_TARGET, ON_TARGET}
+
+#onready
+
+#export
+
+#var
+var target
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -24,5 +34,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
+	
+
+func state_handler():
+	pass
+	
+
+func jump():
+	pass
+	
+
+func apply_target():
+	pass
+	
+
+func apply_magnetism(delta):
+	pass
+	
+
+func camera():
+	pass
+	
