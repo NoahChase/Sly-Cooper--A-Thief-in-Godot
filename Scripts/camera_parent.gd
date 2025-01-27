100f3cc3d62875411c53e0740acd56e84519df14
NoahChase <noah.ryan.chase@gmail.com>
2025-01-26 21:07:45 -0800

    Revert "Camera Adjustments"
    This reverts commit 89ca696aa0abe0fd31f1bd42715798ff2314e8ae.

--- Scripts/camera_parent.gd
+++ Scripts/camera_parent.gd
@@ -59,28 +59,16 @@ func _physics_process(delta):
 
 	pitch = clamp(pitch, deg_to_rad(pitch_min), deg_to_rad(pitch_max))
 	yaw_sens = 1.5
-	pitch_sens = 0.75
+	pitch_sens = 1.5
 
 	if camera_player.direction:
 		if camera_player.floor_or_roof == null or camera_player.floor_or_roof.is_in_group("floor"):
-			pitch = lerp(pitch, -0.2, 0.01)
+			pitch = lerp(pitch, -0.2, 0.015)
 		else:
-<<<<<<< Updated upstream
 			if pitch > 0.2:
 				pitch = lerp(pitch, 0.6, 0.015)
 			else:
 				pitch = lerp(pitch, -0.6, 0.015)
-=======
-<<<<<<< Updated upstream
-			pitch = lerp(pitch, -0.6, 0.015)
-
-=======
-			if pitch > 0.45:
-				pitch = lerp(pitch, 0.45, 0.01)
-			else:
-				pitch = lerp(pitch, -0.6, 0.01)
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 	if camera_player.ray_to_cam.is_colliding():
 		var wall_detect = camera_player.ray_to_cam.get_collider()
 		var wall_distance = wall_detect.position - camera.global_position
